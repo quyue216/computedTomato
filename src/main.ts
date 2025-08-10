cce71ce562bb8895aef2c68b418c9f3c3a46e245
quyue <10676114@qq.com>
2025-08-10 10:45:54 +0800

    feat: 注册图标

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/base.less'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+app.mount('#app');
